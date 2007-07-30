9ae46c05a80d7856eda08c255f322bcae88c31c6
Ricardo SIGNES <rjbs@codesimply.com>
2007-07-30 12:14:47 +0000

    prepare to be criticized

---
+++ lib/Perl/Critic/Tics.pm
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+package Perl::Critic::Tics;
+
+=head1 NAME
+
+Perl::Critic::Tics - policies for things that make me wince
+
+=head1 VERSION
+
+version 0.001
+
+  $Id: Lax.pm 1488 2007-07-28 00:01:30Z rjbs $
+
+=cut
+
+our $VERSION = '0.001';
+
+=head1 DESCRIPTION
+
+The Perl-Critic-Tics distribution includes extra policies for Perl::Critic to
+address a fairly random assortment of things that make me (rjbs) wince.
+
+=head1 WHY TICS?
+
+Take your pick:
+
+=over
+
+=item B<T>hings B<I> B<C>an't B<S>tand
+
+=item They set off my nervous tic.
+
+=back
+
+=head1 AUTHOR
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007 Ricardo SIGNES.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
