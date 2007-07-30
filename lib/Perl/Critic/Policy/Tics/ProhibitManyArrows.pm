c7717a05656201be16d632d04a1af3db68bbd47a
Ricardo SIGNES <rjbs@codesimply.com>
2007-07-30 16:15:08 +0000

    configurability for =>

--- lib/Perl/Critic/Policy/Tics/ProhibitManyArrows.pm
+++ lib/Perl/Critic/Policy/Tics/ProhibitManyArrows.pm
@@ -9,7 +9,7 @@ Perl::Critic::Policy::Tics::ProhibitManyArrows - (this => is => not => good)
 
 =head1 VERSION
 
-version 0.001
+version 0.002
 
 =head1 DESCRIPTION
 
@@ -21,12 +21,27 @@ You are even more not clever if you do this:
 
   my %hash = (key1=>value1=>key2=>value2=>key3=>value3=>);
 
+=head1 CONFIGURATION
+
+There is one parameter for this policy, F<max_allowed>, which specifies the
+maximum number of fat arrows that may appear as item separators.  The default
+is two.  If you really hate the fat arrow, and never want to see it, you can
+set F<max_allowed> to zero and make any occurance of C<< => >> illegal.
+
+Here are some examples of code that would fail with various F<max_allowed>
+values:
+
+  max_allowed    failing code
+  0              (foo => bar)
+  1              (foo => bar => baz)
+  2              (foo => bar => baz => quux)
+
 =cut
 
 use Perl::Critic::Utils;
 use base qw(Perl::Critic::Policy);
 
-our $VERSION = '0.001';
+our $VERSION = '0.003';
 
 my $DESCRIPTION = q{Too many fat-arrow-separated values in a row};
 my $EXPLANATION = q{Fat arrows should separate pairs, not produce long chains
@@ -35,6 +50,22 @@ of values};
 sub default_severity { $SEVERITY_MEDIUM       }
 sub default_themes   { qw(tics)               }
 sub applies_to       { 'PPI::Token::Operator' }
+
+sub supported_parameters { qw(max_allowed) }
+sub new {
+  my ($class, %arg) = @_;
+  my $self = $class->SUPER::new(%arg);
+
+  $arg{max_allowed} = 2 unless defined $arg{max_allowed};
+
+  Carp::croak "max_allowed for Tics::ProhibitManyArrows must be a positive int"
+    unless $arg{max_allowed} =~ /\A\d+\z/ and $arg{max_allowed} >= 0;
+
+  $self->{max_allowed} = $arg{max_allowed};
+  bless $self => $class;
+}
+
+sub max_allowed { $_[0]->{max_allowed} }
 
 sub violates {
   my ($self, $elem, $doc) = @_;
@@ -51,7 +82,7 @@ sub violates {
     $start = $next;
   }
 
-  return unless $in_a_row > 2; # XXX: make it configurable
+  return unless $in_a_row > $self->max_allowed; # XXX: make it configurable
 
   # Must be a violation...
   return $self->violation($DESCRIPTION, $EXPLANATION, $start);
--- t/many-arrows.t
+++ t/many-arrows.t
@@ -5,25 +5,33 @@ use warnings;
 use Perl::Critic::TestUtils qw(pcritique);
 use Test::More;
 
-my @ok = (
-  q{%hash = (key => 'value', key2 => 'value2');},
+my %in_a_row = (
+  0 => q{my %hash = (1, 2, 3, 4);},
+  1 => [
+    q{my %hash = (1 => 2, 3, 4);},
+    q{my %hash = (1 => 2, 3 => 4);},
+  ],
+  2 => q{my %hash = (1 => 2 => 3, 4);},
+  3 => q{%hash = (key => value => key2 => 'value2');},
+  4 => q{%hash = (key => value => key2 => value2 =>);},
 );
 
-my @not_ok = (
-  q{%hash = (key => value => key2 => value2 =>);},
-  q{%hash = (key => value => key2 => 'value2');},
-);
-
-plan tests => @ok + @not_ok;
+plan tests => 11; # XXX: calculate this -- rjbs, 2007-07-30
 
 my $policy = 'Tics::ProhibitManyArrows';
 
-for my $i (0 .. $#ok) {
-  my $violation_count = pcritique($policy, \$ok[$i]);
-  is($violation_count, 0, "nothing wrong with $ok[$i]");
-}
+my $max = (reverse sort keys %in_a_row)[0];
 
-for my $i (0 .. $#not_ok) {
-  my $violation_count = pcritique($policy, \$not_ok[$i]);
-  is($violation_count, 1, "$not_ok[$i] is no good");
+for my $i (0 .. $max) {
+  my @tests = ref $in_a_row{$i} ? @{ $in_a_row{$i} } : $in_a_row{$i};
+
+  for my $code (@tests) {
+    my $viol_count = pcritique($policy, \$code, { max_allowed => $i });
+    is($viol_count, 0, "nothing wrong with $code, max_allowed $i");
+
+    if ($i > 0) {
+      my $viol_count = pcritique($policy, \$code, { max_allowed => $i - 1});
+      ok($viol_count > 0, "violations for $code, max_allowed " . ($i - 1));
+    }
+  }
 }
