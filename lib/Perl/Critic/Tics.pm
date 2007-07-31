523ef4bf4cb34a0482478582b3302b2af9020a6d
Ricardo SIGNES <rjbs@codesimply.com>
2007-07-31 01:16:32 +0000

    prep release

--- lib/Perl/Critic/Tics.pm
+++ lib/Perl/Critic/Tics.pm
@@ -9,13 +9,13 @@ Perl::Critic::Tics - policies for things that make me wince
 
 =head1 VERSION
 
-version 0.002
+version 0.003
 
   $Id$
 
 =cut
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 =head1 DESCRIPTION
 
